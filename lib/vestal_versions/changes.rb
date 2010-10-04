11cb4e2865a463563ebfeb74634ad6fe8edc1ca2
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2010-10-04 22:56:14 +0400

    adding to chain in the changes_between only versions from +using+ which numbers are between +from+ and +to+

--- lib/vestal_versions/changes.rb
+++ lib/vestal_versions/changes.rb
@@ -22,7 +22,7 @@ module VestalVersions
       def changes_between(from, to, using = [])
         from_number, to_number = versions.number_at(from), versions.number_at(to)
         return {} if from_number == to_number
-        chain = (using.present? ? using : versions.between(from, to)).reject(&:initial?)
+        chain = (using.present? ? using.to_a.select{|v| v.number >=  from_number && v.number <= to_number} : versions.between(from, to)).reject(&:initial?)
         return {} if chain.empty?
 
         backward = from_number > to_number
