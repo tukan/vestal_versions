5e839b9dcde4cf7e2e65686856f0ccc40a601f71
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2010-10-04 23:59:45 +0400

    added each_revision, each_revision_in_slice, each_revision_upto_current, each_revision_in_list.
    The idea of methods:
    Iterates over all object revision and yields then into block
    It is more optimal that using revert_to in loop because this methods
    uses only one SQL query to fetch all versions.

--- lib/vestal_versions/reversion.rb
+++ lib/vestal_versions/reversion.rb
@@ -62,12 +62,95 @@ module VestalVersions
         version != last_version
       end
 
+      # Iterates over all object resvision and yields then into block
+      # It is more optimal that using revert_to in loop because this methods
+      # uses only one SQL query to fetch all versions.
+      #
+      # +options+:
+      # +order+: (default +asc+) +asc+ or +desc+. Order of versions to iterate over
+      # +with_current+: (default +true+) set to +false+ if you don't want the current
+      # revision to be included in the iteration.
+      def each_revision(options = {}, &block)
+        each_revision_in_list(self.versions.to_a, options, &block)
+      end
+      
+      # Iterates over all object revision from +from+ to +to+ version.
+      # It is more optimal that using revert_to in loop because this methods
+      # uses only one SQL query to fetch all versions.
+      #
+      # +options+:
+      # +order+: (default +asc+) +asc+ or +desc+. Order of versions to iterate over
+      # +with_current+: (default +true+) set to +false+ if you don't want the current
+      # revision to be included in the iteration.
+      def each_revision_in_slice(from, to, options = {}, &block)
+        each_revision_in_list(versions.between(from, to).to_a, options, &block)
+      end
+      
+      # Iterates over all object revision from the first revision to current revision.
+      # It is more optimal that using revert_to in loop because this methods
+      # uses only one SQL query to fetch all versions.
+      #
+      # +options+:
+      # +order+: (default +asc+) +asc+ or +desc+. Order of versions to iterate over
+      # +with_current+: (default +true+) set to +false+ if you don't want the current
+      # revision to be included in the iteration.
+      def each_revision_upto_current(options = {}, &block)
+        each_revision_upto(versions.before(self.version).to_a, options, &block)
+      end
+      
+      # Iterates over all object revision for versions in the +list+
+      # It is more optimal that using revert_to in loop because this methods
+      # uses only one SQL query to fetch all versions.
+      #
+      # +options+:
+      # +order+: (default +asc+) +asc+ or +desc+. Order of versions to iterate over
+      # +with_current+: (default +true+) set to +false+ if you don't want the current
+      # revision to be included in the iteration.
+      def each_revision_in_list(versions_array, options = {}, &block)
+        options = options.reverse_merge({ :order => :asc, :with_current => true })
+                        
+        current_version = versions_array.to_a.select{|v| v.number == self.version}.first
+        current_version = self.version unless current_version
+        
+        versions_array.delete_if{|v| v.number == self.version} unless options[:with_current]        
+        
+        current_revision = self
+        if options[:order].to_sym == :desc
+          versions_array = versions_array.reverse
+          current_revision.send(:revert_from_to, current_version, versions_array.first, versions_array)
+        else
+          current_revision.send(:revert_from_to, current_version, versions_array.first, versions_array)
+        end
+        
+        yield current_revision
+        
+        (1..versions_array.length-1).each do |index|
+          current_revision.send(:revert_from_to, versions_array[index-1], versions_array[index], versions_array)
+
+          yield current_revision
+        end
+      end
+
       private
         # Clears the cached version number instance variables so that they can be recalculated.
         # Useful after a new version is created.
         def reset_version(version = nil)
           @last_version = nil if version.nil?
           self[:version] ? self.version = version : @version = version
+        end
+        
+        # This is internal method to revert from two revisions without any additional SQL queries.
+        # If +from+ and +two+ are two neighbouring versions then no sql queries are needed. If there are
+        # some versions between them then you could pass them as +using+ and they will be used for
+        # calculating changes between revisions. 
+        def revert_from_to(from, to, using = [])
+          to_number = versions.number_at(to)
+
+          changes_between(from, to, using).each do |attribute, change|
+            write_attribute(attribute, change.last)
+          end
+
+          reset_version(to_number)
         end
     end
   end
